6b8dec7960685de1f448b2640639129355b64b43
nofitbox <nofitbox@gmail.com>
2014-04-24 18:07:41 -0400

    listo!!

--- trackitnow20414/Controllers/HomeController.cs
+++ trackitnow20414/Controllers/HomeController.cs
@@ -3,19 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using trackitnow20414.Models;
 
 namespace GoogleMapsPrueba1.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index(string latitud, string longitud)
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
         {
-            ViewBag.Latitud = latitud;
-            ViewBag.Longitud = longitud;
+            //Para conseguir las ultimas coordenadas
+            var coordenas = (from i in db.Coordenadas orderby i.Id descending select i).FirstOrDefault();
+
+            ViewBag.Latitud = coordenas.Latitud;
+            ViewBag.Longitud = coordenas.Longitud;
+            
 
 
             return View();
         }
+
+        [HttpGet]
+
+        public string Create(string latitud, string longitud)
+        {
+            CoordenadasModels coor = new CoordenadasModels();
+
+            coor.Latitud = latitud;
+            coor.Longitud = longitud;
+            coor.Fecha = DateTime.Now;
+            db.Coordenadas.Add(coor);
+            db.SaveChanges();
+
+            return "1";
+        }
+
+
+
+
 
         public ActionResult About()
         {
